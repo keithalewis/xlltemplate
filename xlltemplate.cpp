08998812ebf4811c42ddd981d056bc49bf68132f
Keith A. Lewis <keithalewis@live.com>
2018-06-03 14:14:15 -0400

    sync

--- xlltemplate.cpp
+++ xlltemplate.cpp
@@ -1,4 +1,18 @@
 // xlltemplate.cpp
+#include <cmath>
 #include "xlltemplate.h"
 
 using namespace xll;
+
+// Information Excel needs to register add-in.
+AddIn xai_function(
+    Function(XLL_DOUBLE, L"?xll_function", L"XLL.FUNCTION")
+    .Arg(XLL_DOUBLE, L"x", L"is the first double argument.")
+    .Category(CATEGORY)
+    .FunctionHelp(L"Help on XLL.FUNCTION goes here.")
+);
+double WINAPI xll_function(double x)
+{
+#pragma XLLEXPORT
+    return exp(x);
+}
