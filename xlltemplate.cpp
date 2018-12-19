d778c770cbb036f061c25d72cb117ecc96cf60e6
kal <kal@kalx.net>
2018-12-19 16:22:43 -0500

    Clean up.

--- xlltemplate.cpp
+++ xlltemplate.cpp
@@ -6,72 +6,35 @@ using namespace xll;
 
 // Information Excel needs to register add-in.
 AddIn xai_function(
-    Function(XLL_DOUBLE, L"?xll_function", L"XLL.FUNCTION")
+	// Function returning a pointer to an OPER with C name xll_function and Excel name XLL.FUNCTION.
+	// Don't forget prepend a question mark to the C name.
+	//                     v
+    Function(XLL_LPOPER, L"?xll_function", L"XLL.FUNCTION")
+	// First argument is a double called x with an argument description.
     .Arg(XLL_DOUBLE, L"x", L"is the first double argument.")
+	// Paste function category.
     .Category(CATEGORY)
     .FunctionHelp(L"Help on XLL.FUNCTION goes here.")
+	.Documentation(LR"(
+Documentation on XLL.FUNCTION goes here.
+    )")
 );
-double WINAPI xll_function(double x)
+// Calling convention *must* be WINAPI (aka __stdcall) for Excel.
+LPOPER WINAPI xll_function(double x)
 {
+// Be sure to export your function.
 #pragma XLLEXPORT
-    return exp(x);
-}
+	static OPER result;
 
-AddIn xai_what(
-    Function(XLL_LPOPER, L"?xll_what", L"XLL.WHAT")
-    .Arg(XLL_LPOPER, L"oper", L"is an reference to a cell or array of cells")
-    .Category(CATEGORY)
-    .FunctionHelp(L"What is the argument?")
-);
-LPOPER WINAPI xll_what(LPOPER po)
-{
-#pragma XLLEXPORT
-    static OPER o;
+	try {
+		ensure(x >= 0);
+		result = sqrt(x); // OPER's act like Excel cells.
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
 
-    switch (po->xltype) {
-    case xltypeNum:
-        o = L"number: ";
-        o = o & *po;
-        break;
-    case xltypeStr:
-        o = L"string: ";
-        o = o & *po;
-        break;
-    case xltypeBool:
-        o = L"boolean: ";
-        o = o & *po;
-        break;
-    case xltypeRef:
-        o = L"reference: ";
-        o = o & Excel(xlfReftext, *po, OPER(true));
-        break;
-    case xltypeErr:
-        o = L"error: ";
-        o = o & *po;
-        break;
-    case xltypeMulti:
-        o = L"multi: ";
-        o = o & Excel(xlfEvaluate, *po);
-        break;
-    case xltypeMissing:
-        o = L"missing";
-        o = o & *po;
-        break;
-    case xltypeNil: // reference to empty cell
-        o = L"nil";
-        o = o & *po;
-        break;
-    case xltypeSRef:
-        o = L"simple reference: ";
-        o = o & Excel(xlfReftext, *po, OPER(true));
-        break;
-    case xltypeInt: // this never happens
-        o = L"integer: ";
-        o = o & *po;
-        break;
-    default:
-        o = L"unknown type";
-    }
+		result = OPER(xlerr::Num);
+	}
 
-    return &o;
+	return &result;
 }
